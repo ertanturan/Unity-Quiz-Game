627ab9e50468d889f46774a89cff895e51715ca9
Ertan Turan <ertanturan@outlook.com>
2020-03-29 18:48:17 +0300

    ad references removed from scripts

--- Assets/GameScripts/StartScript.cs
+++ Assets/GameScripts/StartScript.cs
@@ -1,5 +1,4 @@
-﻿using com.microeyes.admob;
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 
 public class StartScript : MonoBehaviour
@@ -7,22 +6,6 @@ public class StartScript : MonoBehaviour
 
     void Start()
     {
-        //BannerAd l_newBanner = AdmobManager.Prepare<BannerAd>("Banner-1");
-        //l_newBanner.AdUnitId = "ca-app-pub-6065714673957825/1329600461";
-        ////l_newBanner.SetPosition(EPosition.CENTER_HORIZONTAL, EPosition.TOP);
-
-        //l_newBanner.BannerType = EBannerAdType.BANNER;
-        //l_newBanner.Orientation = EOrientation.HORIZONTAL;
-        //l_newBanner.AnimInType = EAnimationInType.FADE_IN;
-        //l_newBanner.AnimOutType = EAnimationOutType.SLIDE_OUT_RIGHT;
-        //l_newBanner.SetPosition(EPosition.CENTER, EPosition.BOTTOM);
-
-        //l_newBanner.Create();
-
-        //l_newBanner.Load();
-
-        //Intestinal_Ad_Create ();
-
         StartCoroutine("MoveScreen");
     }
 
@@ -35,29 +18,6 @@ public class StartScript : MonoBehaviour
         yield return new WaitForSeconds(4);
         Application.LoadLevel("Title");
     }
-    public void Intestinal_Ad_Create()
-    {
-        InterstitialAd l_interAd = AdmobManager.PrepareInterstitialAd("Interstitial_1");
-        l_interAd.AdUnitId = "ca-app-pub-6065714673957825/2806333668";
-        l_interAd.Create();
-    }
-    public void Intestinal_Ad_Load()
-    {
-        InterstitialAd l_interAd = AdmobManager.Get<InterstitialAd>("Interstitial_1");
-        l_interAd.Load();
-    }
-    public void Intestinal_Ad_Show()
-    {
-        InterstitialAd l_interAd = AdmobManager.Get<InterstitialAd>("Interstitial_1");
-        l_interAd.Show();
-    }
 
-    /*IEnumerator Intestinal_Ad() {
-		Intestinal_Ad_Create ();
-		yield return new WaitForSeconds(2);
-		Intestinal_Ad_Load ();
-		yield return new WaitForSeconds(3);
-		Intestinal_Ad_Show ();
-		Intestinal_Ad_Show ();
-	}*/
+
 }
